845f4b0f9db36a3a5ce4c61934755ce5ba9543f5
AiramLemes <airam.lemes101@alu.ulpgc.es>
2023-12-26 11:47:18 +0000

    config: project global config

---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+// Colors.js
+
+const Colors = {
+    
+    primary: '#FDDD80',
+    secondary: '#2ecc71',
+
+    textPrimary: '#000000',
+    textSecondary: '#777',
+  
+    // Colores de fondo
+    background: '#fff',
+    backgroundSecondary: '#f2f2f2',
+  
+    // Otros colores
+    success: '#28a745',
+    error: '#dc3545',
+    warning: '#ffc107',
+  };
+  
+  export default Colors;
+  
